2cb026acd58799b00ceb8ccc14ceb482857214ca
Iosif Karkanis <joskark@hotmail.com>
2016-04-26 19:52:30 +0200

    Pretify list for frontend

--- public/js/controllers/main.js
+++ public/js/controllers/main.js
@@ -4,7 +4,13 @@ angular.module('notificationController', [])
 	.controller('mainController', ['$scope','$http','Notifications', function($scope, $http, Notifications) {
 		$scope.formData = {};
 		$scope.loading = true;
+		$scope.orderByField = 'date';
+		$scope.reverseSort = true;
 
+		$scope.selectedRow = null;  // initialize our variable to null
+		$scope.setClickedRow = function(index){  //function that sets the value of selectedRow to current index
+			$scope.selectedRow = index;
+		}
 		// GET =====================================================================
 		// when landing on the page, get all notifications and show them
 		// use the service to get all the notifications
